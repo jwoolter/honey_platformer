65ffa2af8e954b6e9c4975696c6bb4449f3e4a61
kwoolter <keith.woolterton@gmail.com>
2020-05-22 14:52:07 +0800

    made some improvements

--- Honey platform.py
+++ Honey platform.py
@@ -10,49 +10,59 @@ def draw(surface, data):
 
 
 def main():
-    mygame = model.Game("Rabbit Quest", 300, 300)
+    width = 300
+    height = 300
+
+    mygame = model.Game("Rabbit Quest", width, height)
     os.environ["SDL_VIDEO_CENTERED"] = "1"
     pygame.init()
     pygame.display.set_caption("Honey's adbenture")
     FPSCLOCK = pygame.time.Clock()
+
     PLAYERMOVEX = 2
     PLAYERMOVEY = 10
-    surface = pygame.display.set_mode((300, 300))
+
+    surface = pygame.display.set_mode((width, height))
     x = 100
     y = 100
     data = [x, y, 30, 30]
+
     loop = True
+
     while loop is True:
+
+        dx = dy = 0
+
         for event in pygame.event.get():
+
             if event.type == KEYUP:
                 if event.key == K_ESCAPE:
                     loop = False
 
             elif event.type == QUIT:
                 loop = False
+
             elif event.type == KEYDOWN:
                 if event.key in [K_w, K_UP]:
-                    mygame.move(0, -10)
-                    mygame.update()
+                    dy = -10
+
         keys = pygame.key.get_pressed()
 
         if keys[K_d] or keys[K_RIGHT]:
-            mygame.move(PLAYERMOVEX, 0)
+            dx = PLAYERMOVEX
 
         elif keys[K_a] or keys[K_LEFT]:
-            mygame.move(-1 * PLAYERMOVEX, 0)
+            dx = 0 - PLAYERMOVEX
 
+        mygame.move(dx,dy)
         mygame.update()
 
-        mygame.move(0, 0.5)
-        mygame.update()
-
-        draw(surface, data)
+        surface.fill((30, 30, 30))
         mygame.blocks.draw(surface)
         mygame.sprites.draw(surface)
         pygame.display.update()
 
-        FPSCLOCK.tick(60)
+        FPSCLOCK.tick(30)
 
     pygame.quit()
 
--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 from pygame.locals import *
+
 black = (0,0,0)
 white = (255,255,255)
 blue = (0,0,255)
@@ -12,24 +13,25 @@ class Wall (pygame.sprite.Sprite):
         self.image.fill(blue)
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+
 class MySprite(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("rabbit.png")
-
         self.rect = self.image.get_rect()
 
 
 class Game():
 
     def __init__(self, name, height, width):
+
         self.name = name
         self.height = height
         self.width = width
         self.rect = pygame.Rect(0, 0, width, height)
         self.dx = 0
         self.dy = 0
-        self.gravity = 3
+        self.gravity = 1
         self.sprites = pygame.sprite.Group()
         self.blocks = pygame.sprite.Group()
         self.add_blocks()
@@ -41,30 +43,61 @@ class Game():
             newsprite.rect.x = random.randint(0, self.width)
             newsprite.rect.y = random.randint(0, self.height)
             self.sprites.add(newsprite)"""
+
     def add_blocks(self):
+
         newWall = Wall (50,50, 50, 50)
         self.blocks.add (newWall)
         newWall = Wall (20,20, 50, 50)
         self.blocks.add (newWall)
         newWall = Wall (150,190, 50, 50)
         self.blocks.add (newWall)
+
     def move(self, dx,dy):
+
         self.dx = dx
-        self.player.rect.x += dx
-        self.player.rect.y += dy
         self.dy = dy
+
     def update(self):
+
+        self.gravity_calc()
+
+        self.player.rect.x += self.dx
+
         block_hit_list = pygame.sprite.spritecollide(self.player, self.blocks, False)
         for block in block_hit_list:
             print("hit something")
+
             if self.dx > 0:
                 self.player.rect.right = block.rect.left
             elif self.dx <0:
-                # Otherwise if we are moving left, do the opposite.
                 self.player.rect.left = block.rect.right
+
+            self.dx = 0
+
+        self.player.rect.y += self.dy
+        block_hit_list = pygame.sprite.spritecollide(self.player, self.blocks, False)
+
+        for block in block_hit_list:
+            print("hit something")
 
             if self.dy > 0:
                 self.player.rect.bottom = block.rect.top
             elif self.dy<0:
-                # Otherwise if we are moving left, do the opposite.
                 self.player.rect.top = block.rect.bottom
+
+            self.dy = 0
+
+
+    def gravity_calc(self):
+
+        if self.dy == 0:
+            self.dy = 1
+        else:
+            self.dy += self.gravity
+
+
+        # See if we are on the ground.
+        if self.player.rect.bottom >= self.rect.bottom:
+            #self.dy = 0
+            self.player.rect.bottom = self.rect.bottom
